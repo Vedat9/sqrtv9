8c9e6176987b3cc9fa9941e19e992e6526cd358d
Vedat9 <41365048+Vedat9@users.noreply.github.com>
2022-04-18 15:58:06 +0100

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+/*
+size_t power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return a * power(a, b - 1);
+    }
+}
+*/
+
+long double sqrtv9()
+{
+    long double num{};
+    long double guess{0};
+    long double compare{};
+    int i{1};
+
+    std::cout << "Number? " << std::endl;
+    std::cin >> num;
+    std::cout << std::setprecision(14);
+
+    if (num < 0)
+    {
+        return 0.0 / 0.0;
+    }
+    else if (num == 0)
+    {
+        return 0;
+    }
+    else if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        guess = num / 2;
+        while ((guess * guess) != num)
+        {
+            if ((guess * guess) > num)
+            {
+                guess = guess / 2;
+            }
+            else if ((guess * guess) < num)
+            {
+                guess = guess + guess / 2;
+                while ((guess * guess) != num)
+                {
+                    if ((guess * guess) > num)
+                    {
+                        i++;
+                        guess = guess - guess / pow(2, i);
+                    }
+                    else if ((guess * guess) < num)
+                    {
+                        i++;
+                        guess = guess + guess / pow(2, i);
+                    }
+                    if (compare == guess)
+                    {
+                        num = guess * guess;
+                    }
+                    compare = guess;
+                }
+            }
+        }
+    }
+    std::cout << "Answer: ";
+    return guess;
+}
+
+int main()
+{
+    std::cout << sqrtv9() << std::endl;
+    system("pause");
+    return 0;
+}
